a28fd10cae6a2359c562c9979c3f5355bec7a90c
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2018-08-31 17:55:40 +0700

    :tada: Cool shit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Title<span v-if="isTyping"> หิวมาก</span></h1>
+    <p>Description</p>
+    <input v-model="cooltext"/>
+    <p class="cool" v-bind:style="{ 'color' : color }">{{ cooltext }}</p>
+    <div class="อาหารBox">
+      <div class="อาหาร" v-for="food in myFavoriteFood">{{ food }}</div>
+    </div>
+    <button @click="changeColor">เปลี่ยนสีหน่อย</button>
   </div>
 </template>
 
 <script>
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      cooltext: "",
+      color: "#000",
+      myFavoriteFood: ["pizza", "เหนี่ยวไก่", "อะไรก็ได้อร่อยๆ"]
+    };
+  },
+  methods: {
+    changeColor() {
+      this.color = getRandomColor();
+      this.myFavoriteFood.push(this.cooltext);
+      this.cooltext = "";
+    }
+  },
+  computed: {
+    isTyping() {
+      return this.cooltext !== "";
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.อาหาร {
+}
+.อาหารBox {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+* {
+  font-family: sans-serif;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +73,15 @@ li {
 }
 a {
   color: #42b983;
+}
+.cool {
+  transition: all 1s ease;
+  font-size: 7em;
+  color: purple;
+}
+.cool:hover {
+  text-shadow: 10px 10px 10px black;
+  text-shadow: 5px 5px 5px red;
+  transform: translate(-10px, -10px);
 }
 </style>
